c0782ba79963431df74959efb5735d39c03473c5
Matias Galleguillos <matias.gurrutia@gmail.com>
2024-09-01 13:36:01 -0400

    Se agrega el contador para cada tipo de productos, se diferencia por las fecha de cada productos

--- src/contexts/ItemContext.jsx
+++ src/contexts/ItemContext.jsx
@@ -1,18 +1,31 @@
 import { createContext, useState } from "react";
 
-// Crea el contexto para los ítems
 export const ItemContext = createContext();
 
-// Crea el proveedor del contexto
 export const ItemProvider = ({ children }) => {
   const [items, setItems] = useState([]);
 
-  // Función para agregar un ítem al carrito
-  const addItem = (item) => {
-    setItems((prevItems) => [...prevItems, item]);
+  const addItem = (newItem) => {
+    const existingItemIndex = items.findIndex(
+      (item) => item.id === newItem.id && item.fechaSeleccionada === newItem.fechaSeleccionada
+    );
+
+    if (existingItemIndex !== -1) {
+      // Si el producto ya existe en el carrito con la misma fecha, incrementa la cantidad
+      const updatedItems = items.map((item, index) =>
+        index === existingItemIndex
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      );
+      setItems(updatedItems);
+    } else {
+      // Si es un producto nuevo (diferente fecha o no existe en el carrito)
+      setItems([...items, { ...newItem, quantity: 1 }]);
+    }
   };
 
-  const reset = () => setItems([] );
+  const reset = () => setItems([]);
+
   return (
     <ItemContext.Provider value={{ items, addItem, reset }}>
       {children}
